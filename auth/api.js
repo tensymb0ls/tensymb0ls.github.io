fceaca7a61645f741b001612e3d89b24e01bc029
tensymb0ls <tensym8ols@gmail.com>
2023-11-23 15:43:30 +0200

    acc_token

--- auth/api.js
+++ auth/api.js
@@ -1,15 +1,22 @@
-(function ($) {
-    $(window).on('load', function () {
-        $.instagramFeed({
-            'username': 'ten.symbols',
-            'container': "#instafeed",
-            'display_profile': false,
-            'display_biography': false,
-            'display_gallery': true,
-            'styling': true,
-            'items': 5,
-            'items_per_row': 5,
-            'margin': 0.5
+const accessToken = 'YOUR_ACCESS_TOKEN';
+const userId = 'YOUR_USER_ID';
+
+async function fetchInstagramPhotos() {
+    try {
+        const endpoint = `https://graph.instagram.com/${userId}/media?fields=id,media_type,media_url&access_token=${accessToken}`;
+        const response = await fetch(endpoint);
+        const data = await response.json();
+
+        const photosDiv = document.getElementById('photos');
+        data.data.filter(photo => photo.media_type === 'IMAGE').forEach(photo => {
+            const img = document.createElement('img');
+            img.src = photo.media_url;
+            img.style.width = '300px';
+            photosDiv.appendChild(img);
         });
-    });
-})(jQuery);
+    } catch (error) {
+        console.error('Ошибка получения данных:', error);
+    }
+}
+
+fetchInstagramPhotos();
