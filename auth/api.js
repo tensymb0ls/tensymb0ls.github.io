983bd4911204f2997962759bd447fd5eea6edaa3
tensymb0ls <tensym8ols@gmail.com>
2023-11-23 13:21:56 +0200

    edit

--- auth/api.js
+++ auth/api.js
@@ -1,15 +1,15 @@
 (function ($) {
     $(window).on('load', function () {
         $.instagramFeed({
-            'username': 'ten.symbols', //Имя пользователя
-            'container': "#instafeed", //Контейнер для изображений
-            'display_profile': false, //Отображение профиля
-            'display_biography': false, //Отображение биографии
-            'display_gallery': true, //Отображение галереи
-            'styling': true, //Стандартные стили библиотеки
-            'items': 5, //Количество изображений в галереи
-            'items_per_row': 5, //Количество изображений в ряд
-            'margin': 0.5 //Отступ между изображениями
+            'username': 'ten.symbols',
+            'container': "#instafeed",
+            'display_profile': false,
+            'display_biography': false,
+            'display_gallery': true,
+            'styling': true,
+            'items': 5,
+            'items_per_row': 5,
+            'margin': 0.5
         });
     });
 })(jQuery);
